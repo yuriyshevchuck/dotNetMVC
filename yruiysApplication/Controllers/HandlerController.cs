e782e283716ecb254605aed819d0edc61a1a625f
yuriyshevchuck <yuriy.shevchuck@gmail.com>
2017-10-02 20:23:04 +0100

    initial commit of aspnetMVC

---
+++ yruiysApplication/Controllers/HandlerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace yruiysApplication.Controllers
+{
+    public class HandlerController : Controller
+    {
+        // GET: Handler
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Test()
+        {
+            return "Hello world";
+        }
+    }
+}
---
+++ yruiysApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(yruiysApplication.Startup))]
+namespace yruiysApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
